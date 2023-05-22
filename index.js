d841c327c9b7a7ef75962dedfff7ee0226fddf9a
Anderson Casimiro <97445047+cassgkn@users.noreply.github.com>
2023-05-21 22:52:30 -0300

    Atualização resposta

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function calcularCombustivel() {
     
     var razao = (valorEtanol / valorGasolina) * 100;
     if (razao <= 70) {
-      document.getElementById('resultado').innerHTML = `Abasteça com Etanol! Pois a diferença está c/ ${razao.toFixed(0)}% menor ou igual em relação c/ o preço da Gasolina.`;
+      document.getElementById('resultado').innerHTML = `Abasteça com Etanol, essa é a melhor opção!`;
     } else {
-      document.getElementById('resultado').innerHTML = `Não abasteça com Alcóol! Pois a diferença está c/ ${razao.toFixed(0)}% em relação c/ o preço da Gasolina.`;
+      document.getElementById('resultado').innerHTML = `Abasteça com Gasolina, essa é a melhor opção!`;
     }
   }
 
